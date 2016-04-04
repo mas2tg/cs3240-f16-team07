2f6b8c74246abe647765c5fd1d62e1cd04279de6
Zheng Qin <zq2sa@virginia.edu>
2016-04-04 10:11:42 -0400

    kevin 4.4 ver2

--- dev/safecollab/reports/models.py
+++ dev/safecollab/reports/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from users.models import User
 
+
+
 class Report(models.Model):
 	creator = models.ForeignKey(User, default=None, null=True)			# User ID of user who submitted report
 	encrypted = models.BooleanField(default=False)
-
+	path = models.FileField()
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=300)
 class Attachment(models.Model):
 	report_id = models.ForeignKey(Report)
 	url = models.CharField(max_length=100)
--- dev/safecollab/reports/views.py
+++ dev/safecollab/reports/views.py
@@ -1,3 +1,45 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from reports.models import Report
+from reports.forms import ReportForm
 
-# Create your views here.
+
+def index(request):
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
+
+    category_list = Report.objects.all()
+    context_dict = {'reports': category_list}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'showReports.html', context_dict)
+
+
+def add_report(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = ReportForm(request.POST, request.FILES)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print (form.errors)
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = ReportForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'add_report.html', {'form': form})
+
