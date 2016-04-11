0a5697cd295b1b04ab54bb83e2c985c167d04575
Nathan <nlw2sx@virginia.edu>
2016-04-11 01:26:17 -0400

    Completed Messages application

--- dev/safecollab/inbox/views.py
+++ dev/safecollab/inbox/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from inbox.models import Message
-import json
+from crypto.models import AESCipher
+import os
 
 def index(request):
 	return render(request, 'inbox.html', {})
@@ -12,8 +13,14 @@ def send(request):
 		sender = request.user
 		recipient = User.objects.get(username=request.POST.get('recipient'))
 		body = request.POST.get('body')
+		encrypt = True if request.POST.get('encrypt') else False
+		key = None
 
-		message = Message(sender=sender, recipient=recipient, body=body)
+		if encrypt:
+			key = os.urandom(32)
+			body = AESCipher(key).encrypt(body).decode('utf-8')
+
+		message = Message(sender=sender, recipient=recipient, body=body, encrypted=encrypt, key=key)
 		message.save()
 
 		return HttpResponseRedirect('/inbox')
@@ -26,13 +33,17 @@ def delete(request):
 	if request.method == 'POST':
 		message_ids = request.POST.getlist('message_ids[]')
 		for message_id in message_ids:
+			print(message_id)
+			Message.objects.get(id=message_id)
+			print('test')
 			Message.objects.get(id=message_id).delete()
 		
+		print('test')
 		unread = request.user.get_unread_messages()
 		data = {
 			'message_count': unread.count(),
 		}
-		return HttpResponse(json.dumps(data))
+		return JsonResponse(data)
 
 	else:
 		# This should never happen
@@ -49,7 +60,7 @@ def mark_as_read(request):
 		data = {
 			'message_count': unread.count(),
 		}
-		return HttpResponse(json.dumps(data))
+		return JsonResponse(data)
 
 	else:
 		# This should never happen
@@ -67,7 +78,21 @@ def mark_as_unread(request):
 		data = {
 			'message_count': unread.count(),
 		}
-		return HttpResponse(json.dumps(data))
+		return JsonResponse(data)
+
+	else:
+		# This should never happen
+		return HttpResponse('Improper arrival at mark_as_unread, see inbox.views')
+
+def decrypt(request):
+	if request.method == 'POST':
+		message_id = request.POST.get('message_id')
+		message = Message.objects.get(id=message_id)
+		raw = AESCipher(bytes(message.key)).decrypt(message.body).decode('utf-8')
+		data = {
+			'raw': raw,
+		}
+		return JsonResponse(data)
 
 	else:
 		# This should never happen
