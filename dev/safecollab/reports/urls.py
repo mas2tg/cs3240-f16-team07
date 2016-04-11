cd8e756715a4728de81628297f20217f81896bbe
Zheng Qin <zq2sa@virginia.edu>
2016-04-11 02:18:06 -0400

    4.10 Kevin

--- dev/safecollab/reports/urls.py
+++ dev/safecollab/reports/urls.py
@@ -4,4 +4,5 @@ from reports import views
 urlpatterns = patterns('',
 	url(r'^add-report', views.add_report, name='add-report'),
     url(r'^$', views.index, name='index'),
+    url(r'^attachments/(?P<link_to_file>.*)/$', views.download,name='download')
 )
--- dev/safecollab/reports/views.py
+++ dev/safecollab/reports/views.py
@@ -1,15 +1,30 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from reports.models import Report, ReportForm
+from django.db.models import Q
+from django.utils.encoding import smart_str
+from django.http import StreamingHttpResponse
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 def index(request):
+    username = request.user
+    category_list = Report.objects.filter(creator=username)
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by no. likes in descending order.
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
 
-    category_list = Report.objects.all()
-    context_dict = {'reports':category_list, }
+    queryType = request.GET.get("type")
+    query = request.GET.get("q")
+    if(query):
+        if (queryType=="name"):
+
+            category_list = category_list.filter(Q(name__icontains=query))
+        elif (queryType=="description"):
+            category_list = category_list.filter(Q(description__icontains=query))
+
+    context_dict = {'reports':category_list,"type":queryType}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
@@ -36,3 +51,20 @@ def add_report(request):
         return HttpResponse('Improper arrival at add_report, see reports.views')
 
 
+def download(request,link_to_file):
+      path = os.path.join(BASE_DIR, 'media')
+      path += "/attachments"
+      filename=link_to_file
+
+#       response = HttpResponse(content_type='application/force-download')
+#       response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(link_to_file)
+#       response['X-Sendfile'] = smart_str(path)
+# # It's usually a good idea to set the 'Content-Length' header too.
+# # You can also set any other required headers: Cache-Control, etc.
+#       return response
+      file_full_path = path+"/{0}".format(filename)
+
+      response = StreamingHttpResponse((line for line in open(file_full_path,'r')))
+      response['Content-Disposition'] = "attachment; filename={0}".format(filename)
+      response['Content-Length'] = os.path.getsize(file_full_path)
+      return response
